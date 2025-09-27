1cf1989b8d1ba2c23a5b58e6b316894c5ef4d09e
Bekarys <241870@astanait.edu.kz>
2025-09-27 17:29:45 +0500

    Add files via upload

---
+++ AdapterPattern/src/Mercedes.java
@@ -0,0 +1,6 @@
+class Mercedes implements Car {
+    @Override
+    public void drive() {
+        System.out.println("Mercedes drive");
+    }
+}
---
+++ AdapterPattern/src/OldCarSystem.java
@@ -0,0 +1,5 @@
+public class OldCarSystem {
+    public void moveCar(){
+        System.out.println("OldCarSystem:The car drives using the old system!");
+    }
+}
